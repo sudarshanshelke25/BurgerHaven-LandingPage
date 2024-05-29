ec92dfcd2e19fc5b02c4bf9ed6ff80c2e75d95cd
SudarshanShelke <sudarshanshelke@gmail.com>
2024-05-30 01:37:43 +0530

    1st Commit: Create LandingPage for BurgerHaven

---
+++ script.js
@@ -0,0 +1,38 @@
+const menuBtn = document.getElementById("menu-btn"); 
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-large-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header-image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header-content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
